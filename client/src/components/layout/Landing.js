2494202dbd5b0105bc0e367041af69b75cdfb235
John Kozyrakis <jckozy86@gmail.com>
2019-07-30 11:47:00 -0400

    Copied styling in login and register page and added login/register icons as well

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSignInAlt, faUserPlus } from '@fortawesome/free-solid-svg-icons';
 
 class Landing extends Component {
   render() {
     return (
-      <div style={{ height: "75vh" }} className="container valign-wrapper">
+      <div style={{ height: "75vh" }} className="container-fluid valign-wrapper">
         <div className="row">
           <div className="col s12 center-align">
             <h4>
@@ -19,13 +21,17 @@ class Landing extends Component {
               <Link
                 to="/register"
                 style={{
-                  width: "140px",
-                  borderRadius: "3px",
-                  letterSpacing: "1.5px",
-                  color: "black",
+                  width: "25vw",
+                  height: "6vh",
+                  //borderRadius: "3px",
+                  // letterSpacing: "1.5px",
+                  // color: "black",
                 }}
                 className="btn btn-large waves-effect waves-light hoverable yellow accent-3"
               >
+                <span style={{ marginRight: "1rem" }}>
+                  <FontAwesomeIcon icon={faUserPlus} size="lg" />
+                </span>
                 Register
               </Link>
             </div>
@@ -33,13 +39,17 @@ class Landing extends Component {
               <Link
                 to="/login"
                 style={{
-                  width: "140px",
-                  borderRadius: "3px",
-                  letterSpacing: "1.5px",
-                  color: "black",
+                  width: "25vw",
+                  height: "6vh",
+                  // borderRadius: "3px",
+                  // letterSpacing: "1.5px",
+                  // color: "black",
                 }}
                 className="btn btn-large waves-effect waves-light hoverable yellow accent-3"
               >
+                <span style={{ marginRight: "1rem" }}>
+                <FontAwesomeIcon icon={faSignInAlt} size="lg" />
+                </span>
                 Log In
               </Link>
             </div>
