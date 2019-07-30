e5141f3d7fe22f737bacf86a8070e9d99e280be9
John Kozyrakis <jckozy86@gmail.com>
2019-07-30 11:20:45 -0400

    fixed buttons to be reactive and centered to page with logout underneath, also made hover to be with opacity 40% in videos

--- client/src/components/video/Youtube.js
+++ client/src/components/video/Youtube.js
@@ -11,6 +11,8 @@ import VideoDetail from './video_detail';
 import YTSearch from 'youtube-api-search';
 //importing logoutuser functionality
 import { logoutUser } from "../../actions/authActions";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSignOutAlt } from '@fortawesome/free-solid-svg-icons'
 
 class video extends Component {
   constructor(props) {
@@ -32,12 +34,12 @@ class video extends Component {
 
         this.videoSearch('Motivational Videos', res.data.YOUTUBE_API_KEY);
 
-          this.setState({
-              API_KEY: res.data.YOUTUBE_API_KEY
-          })
+        this.setState({
+          API_KEY: res.data.YOUTUBE_API_KEY
+        })
       })
       .catch(err => {
-          console.log("Errored out when getting task data: " + err);
+        console.log("Errored out when getting task data: " + err);
       });
   }
 
@@ -48,13 +50,13 @@ class video extends Component {
 
   videoSearch(searchTerm, key) {
 
-      YTSearch({ key: key, term: searchTerm }, (data) => {
-        //console.log(data);
-        this.setState({
-          videos: data,
-          selectedVideo: data[0]
-        });
+    YTSearch({ key: key, term: searchTerm }, (data) => {
+      //console.log(data);
+      this.setState({
+        videos: data,
+        selectedVideo: data[0]
       });
+    });
 
   }
 
@@ -64,80 +66,94 @@ class video extends Component {
 
     return (
 
-      (typeof API_KEY === 'undefined' || API_KEY === null || API_KEY === '')?
+      (typeof API_KEY === 'undefined' || API_KEY === null || API_KEY === '') ?
         null
-      :
-      <>
+        :
         <>
-          <div style={{ marginTop: "4rem", "fontSize": "0.5vw" }} className="row ">
-            <div className="col s10 m6 l4">
-              <Link to="/dashboard" className="btn-flat waves-effect">
-                <i className="material-icons ">keyboard_backspace</i>
-                Back to dashboard
+          <>
+            <div style={{ marginTop: "4rem", "fontSize": "0.5vw" }} className="row ">
+              <div className="col s10 m6 l4">
+                <Link to="/dashboard" className="btn-flat waves-effect">
+                  <i className="material-icons ">keyboard_backspace</i>
+                  Back to dashboard
                         </Link>
+              </div>
+            </div>
+          </>
+          <div className="container-fluid">
+            <div className="row">
+              <div className="col s12">
+                <SearchBar onSearchTermChange={searchTerm => this.videoSearch(searchTerm, API_KEY)} />
+              </div>
+              <div className="col s12 m12 l8">
+                <VideoDetail video={this.state.selectedVideo} />
+              </div>
+              <div className="col s12 m12 l4">
+                <VideoList
+                  onVideoSelect={userSelected => this.setState({ selectedVideo: userSelected })}
+                  videos={this.state.videos}
+                />
+              </div>
+            </div>
+          </div>
+          <div className="container valign-wrapper center-align">
+            <div className="row">
+              <div style={{ display: "inline-block" }} className="landing-copy col 12 center-align">
+                <Link
+                  key="achievement_btn"
+                  to="/achievements"
+                  style={{
+                    width: "17vw",
+                    //borderRadius: "3px",
+                    //height: "6vh",
+                    //letterSpacing: "1.5px",
+                    //color: "black",
+                    //margin: "5px",
+                  }}
+                  className="btn waves-effect waves-light hoverable yellow accent-3"
+                >
+                  Achievements
+							</Link>
+                <Link
+                  key="task_btn"
+                  to="/tasks"
+                  style={{
+                    width: "12vw",
+                    //borderRadius: "3px",
+                    //height: "6vh",
+                    //letterSpacing: "1.5px",
+                    //color: "black",
+                    //margin: "5px"
+                  }}
+                  className="btn waves-effect waves-light hoverable yellow accent-3"
+                >
+                  Tasks
+                                </Link>
+              </div>
+              <div >
+                <Link
+                  key="logout"
+                  to="#"
+                  style={{
+                    width: "22vw",
+                    //borderRadius: "3px",
+                    //letterSpacing: "1.5px",
+                    marginTop: "1rem",
+                    //color: "black",
+                    //margin: "10px"
+                  }}
+                  onClick={this.onLogoutClick}
+                  className="btn waves-effect waves-light hoverable yellow accent-3"
+                >
+                  <span style={{ marginRight: "1rem" }}>
+                    <FontAwesomeIcon icon={faSignOutAlt} size="2x" />
+                  </span>
+                  Logout
+                                </Link>
+              </div>
             </div>
           </div>
         </>
-        <div className="container-fluid">
-          <div className="row">
-            <div className="col s12">
-              <SearchBar onSearchTermChange={searchTerm => this.videoSearch(searchTerm, API_KEY)} />
-            </div>
-            <div className="col s12 m12 l8">
-              <VideoDetail video={this.state.selectedVideo} />
-            </div>
-            <div className="col s12 m12 l4">
-              <VideoList
-                onVideoSelect={userSelected => this.setState({ selectedVideo: userSelected })}
-                videos={this.state.videos}
-              />
-            </div>
-          </div>
-        </div>
-        <div style={{ display: "inline-block" }} className="">
-          <Link
-            key="achievement_btn"
-            to="/achievements"
-            style={{
-              width: "195px",
-              borderRadius: "3px",
-              letterSpacing: "1.5px",
-              color: "black",
-              margin: "5px",
-            }}
-            className="btn btn-large waves-effect waves-light hoverable yellow accent-3">
-            Achievements
-              </Link>
-          <Link
-            key="task_btn"
-            to="/tasks"
-            style={{
-              width: "235px",
-              borderRadius: "3px",
-              letterSpacing: "1.5px",
-              color: "black",
-              margin: "5px"
-            }}
-            className="btn btn-large waves-effect waves-light hoverable yellow accent-3">
-            Earn Stars
-          </Link>
-        </div>
-        <div></div>
-        <button
-          style={{
-            width: "150px",
-            borderRadius: "3px",
-            letterSpacing: "1.5px",
-            marginTop: "1rem",
-            color: "black",
-            margin: "10px"
-          }}
-          onClick={this.onLogoutClick}
-          className="btn btn-large waves-effect waves-light hoverable yellow accent-3"
-        >
-          Logout
-            </button>
-      </>
     );
   }
 
